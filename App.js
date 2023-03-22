104fca321ba1bcac9b71844aa59c1e0d498080a9
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-22 10:50:42 -0300

    Capa_livro

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+// import Card from "./src/components/Card";
+
+
+function Card(props) {
+  return(
+    <View style={styles.card}>
+      <Image style={styles.livro} source={{uri: props.livro.capa}} />
+      <Text style={{color: 'darkred' }} > {props.livro.titulo} </Text>
+      <Text style={{fontSize: 12 }}> R$ {props.livro.preco}</Text>
+
+    </View>
+  );
+}
 
 export default function App() {
+  const livros = [
+    {
+      capa: 'https://reactnative.dev/img/tiny_logo.png',
+      titulo: 'React native',
+      preco: "100,00"
+    }
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 25, fontWeight: 'bold' }}> Img do CAPETA!!</Text>
+      <View style={styles.conteudo}>
+        { livros.map(livro => <Card livro={livro} />) }
+      </View>
     </View>
   );
 }
@@ -13,8 +35,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 20,
+    backgroundColor: "darkred",
+},
+
+conteudo: {
+  flex: 1,
+  backgroundColor: 'green',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  width:'100%',
+},
+
+card: {
+  backgroundColor: 'pink',
+  width: '40%',
+  height: 215,
+  borderRadius: 10,
+  margin: 15
+},
+
+livro:{
+  resizeMode: 'stretch',
+  width: '100%',
+  height: 180,
+},
+
 });
---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+// import { View, Image, Text } from "react-native";
+// export default function Card() {
+//     return (
+//         <View>
+//             <Image source={require('../../img/imagen_livro.jpg')}/>
+//             <Text>Imagen!!</Text>
+//         </View>
+//     )
+// }
